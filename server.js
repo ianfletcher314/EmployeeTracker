301774e09a98766046f3f6ddaa2288e622f2e77a
Ian Fletcher <ianfletcher314@gmail.com>
2021-04-28 15:38:55 -0500

    if else if statements turned into async functions

--- server.js
+++ server.js
@@ -29,19 +29,26 @@ const start = () => {
         await viewDepartmentsFunction();
         start()
       } else if (answer.choiceEnder === 'View Roles') {
-        viewRolesFunction(answer)
+        await viewRolesFunction(answer)
+        start()
       } else if (answer.choiceEnder === 'View Employees') {
-        viewEmployeesFunction(answer)
+        await viewEmployeesFunction(answer)
+        start()
       } else if (answer.choiceEnder === 'Add Department') {
-        addDepartmentsFunction(answer)
+        await addDepartmentsFunction(answer)
+        start()
       } else if (answer.choiceEnder === 'Add Role') {
-        addRolesFunction(answer)
+        await addRolesFunction(answer)
+        start()
       } else if (answer.choiceEnder === 'Add Employee') {
-        addEmployeesFunction(answer)
+        await addEmployeesFunction(answer)
+        start()
       } else if (answer.choiceEnder === 'Update Employee Role') {
-        updateEmployeeRoleFunction(answer)
+        await updateEmployeeRoleFunction(answer)
+        start()
       } else if (answer.choiceEnder === 'EXIT') {
-        exitFunction(answer)
+        await exitFunction(answer)
+        start()
       }
     });
 };
@@ -102,9 +109,35 @@ const viewDepartmentsFunction = async () => {
 
 
 }
-const viewRolesFunction = (answer) => {
-  console.log(answer, "view Functin ROle")
-  start()
+const viewRolesFunction = async() => {
+  return new Promise (async(resolve,reject) => {
+  const departmentDataArray = await departmentData()
+  // console.log(departmentDataArray)
+  // once it gets the answer it asks which department does the user want to look at
+  inquirer
+    .prompt({
+      name: 'departments',
+      type: 'list',
+      message: 'What department would you like to view.',
+      choices: departmentDataArray,
+    })
+    // once they answer we query our database for a list of employees in the department they want
+    .then((data) => {
+      connection.query(`SELECT CONCAT (employee.first_name, " ", employee.last_name) AS employee, 
+      role.title 
+  FROM employee 
+      LEFT JOIN role ON employee.role_id = role.id 
+      LEFT JOIN department ON department_id = department.id 
+      WHERE department.name = ?`, data.departments,(err,res)=>{
+        if (err) throw err
+        console.log("\nhere are your results for the department you selected\n\n")
+        const departmentResults = console.table(res)
+        resolve(departmentResults)
+      })
+    })
+  })
+
+
 }
 const viewEmployeesFunction = (answer) => {
   console.log(answer, "view employee function")
